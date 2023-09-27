007205b3864679aa5c2e6f2007132bfe832ad6f6
Shashwat Sharma <zshashwatz@gmail.com>
2023-09-27 22:35:44 +0530

    Update __init__.py

--- jaxllama/__init__.py
+++ jaxllama/__init__.py
@@ -1,3 +1,15 @@
+from functools import partial
+import logging
+import warnings
+
+from flash_attention_jax import causal_flash_attention
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import jmp
+
 from .model import LlamaModel, LlamaConfig
 from .load import get_generator, get_model, load_config, load_weights
 from .utils import simple_dtype_policy
+from .config import ConfigModule
+from .decoder import LLamaDecoderLayer
